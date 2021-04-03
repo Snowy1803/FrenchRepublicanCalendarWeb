a1eb94ad321c6842fe524656d60f3aa1ef50bb9b
Snowy <3mille.prenom.nom@gmail.com>
2021-04-03 20:40:18 +0200

    Using the same alignment method on the converter part as in the details selection

--- Sources/FrenchRepublicanCalendarWeb/main.swift
+++ Sources/FrenchRepublicanCalendarWeb/main.swift
@@ -17,36 +17,36 @@ struct ContentView: View {
         HTML("div", ["style": "width: 100%; height: 100%; padding: 10px;"]) {
             VStack(alignment: .leading) {
                 Text("Calendrier républicain").font(.largeTitle)
+                let frd = FrenchRepublicanDate(date: date)
                 HStack {
-                    Text("Aujourd'hui :")
-                    Spacer()
-                    DynamicHTML("a", [
-                        "href": "javascript:void%200",
-                    ], listeners: [
-                        "click": { _ in
-                            date = Date()
+                    VStack(alignment: .trailing) {
+                        Text("Aujourd'hui :")
+                        Text("Grégorien :")
+                        Text("Républicain :")
+                    }.padding([.leading, .trailing], 15)
+                    .frame(width: 150)
+                    
+                    VStack(alignment: .leading) {
+                        DynamicHTML("a", [
+                            "href": "javascript:void%200",
+                        ], listeners: [
+                            "click": { _ in
+                                date = Date()
+                            }
+                        ]) {
+                            Text(FrenchRepublicanDate(date: Date()).toVeryLongString())
+                                .foregroundColor(.accentColor)
                         }
-                    ]) {
-                        Text(FrenchRepublicanDate(date: Date()).toVeryLongString())
-                            .foregroundColor(.accentColor)
+                        DatePicker("", selection: $date, in: FrenchRepublicanDate.safeRange, displayedComponents: [.date])
+                        RepublicanDatePicker(date: Binding {
+                            MyRepublicanDateComponents(day: frd.components.day!, month: frd.components.month!, year: frd.components.year!)
+                        } set: {
+                            date = $0.toRep.date
+                        })
                     }
-                }
-                HStack {
-                    Text("Grégorien :")
                     Spacer()
-                    DatePicker("", selection: $date, in: FrenchRepublicanDate.safeRange, displayedComponents: [.date])
                 }
-                HStack {
-                    Text("Républicain :")
-                    Spacer()
-                    let frd = FrenchRepublicanDate(date: date)
-                    RepublicanDatePicker(date: Binding {
-                        MyRepublicanDateComponents(day: frd.components.day!, month: frd.components.month!, year: frd.components.year!)
-                    } set: {
-                        date = $0.toRep.date
-                    })
-                }
-                DateDetails(date: FrenchRepublicanDate(date: date))
+                DateDetails(date: frd)
                 Spacer()
             }
         }
@@ -67,6 +67,7 @@ struct DateDetails: View {
                 Text("Jour de l'année :")
                 Text("Date abrégée :")
             }.padding([.leading, .trailing], 15)
+            .frame(width: 150)
             
             VStack(alignment: .leading) {
                 Link(destination: date.descriptionURL!) {
